486ef3689fb0b303ccb47f69318f4b8a8cb4b1c2
mnmsnrk <69619645+mnmsnrk@users.noreply.github.com>
2020-09-04 18:17:37 +0900

    単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,:birth_date, presence: true
+  validates :nickname,:email,:first_name,:last_name,:birth_date, presence: true
   
-  VALID_PASSWORD_REGEX =/\A[a-z\d]{6,12}+\z/i
-  validates :password, presence: true,
-            format: { with: VALID_PASSWORD_REGEX,
-             message: "は半角6~12文字英数字それぞれ１文字以上含む必要があります"}
-            
+  validates :password,length:{ minimum: 6 }
   validates :password, confirmation: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -19,6 +15,7 @@ class User < ApplicationRecord
   with_options presence:true do
     validates :first_name,:last_name,format:{ with: /\A[ぁ-んァ-ン一-龥]/,message:"は全角で入力してください。"}
     validates :first_name_kana,:last_name_kana,format:{with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "は半角英数で入力してください。"}
   end
 
 end
