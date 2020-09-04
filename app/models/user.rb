85edbf422768e68d607b6d6e1267ec725cbc5373
mnmsnrk <69619645+mnmsnrk@users.noreply.github.com>
2020-09-04 12:17:31 +0900

    Rspecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,:birth_date, presence: true
+  
+  VALID_PASSWORD_REGEX =/\A[a-z\d]{6,12}+\z/i
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX,
+             message: "は半角6~12文字英数字それぞれ１文字以上含む必要があります"}
+            
+  validates :password, confirmation: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true,format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
+  with_options presence:true do
+    validates :first_name,:last_name,format:{ with: /\A[ぁ-んァ-ン一-龥]/,message:"は全角で入力してください。"}
+    validates :first_name_kana,:last_name_kana,format:{with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_09_02_022834) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "first_name", null: false
