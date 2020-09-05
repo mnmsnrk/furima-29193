6719dac974566adcf675f37c26f8e6ebe1696543
mnmsnrk <69619645+mnmsnrk@users.noreply.github.com>
2020-09-05 17:06:22 +0900

    カテゴリー追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  validates :category_id,:condition_id,:postage_id,:prefecture_id,:handling_id, numericality: { other_than: 1 } 
 
   with_options presence:true do
   validates :name, :image_text, :category_id,:condition_id,:postage_id,:prefecture_id,:handling_id,:price
   end
-  
+
   belongs_to :user
 end
