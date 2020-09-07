25a3453c4b8311dbdf2f38ab72b8db22a62deffd
mnmsnrk <69619645+mnmsnrk@users.noreply.github.com>
2020-09-07 16:42:59 +0900

    出品完了後トップページへ遷移

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage
-
+  belongs_to_active_hash :handling
+  belongs_to_active_hash :prefecture
+  
   validates :category_id,:condition_id,:postage_id,:prefecture_id,:handling_id, numericality: { other_than: 1 } 
 
   with_options presence:true do
