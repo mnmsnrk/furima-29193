ddc122609a72ad12553dcf08ce5cc3dd7647a2bf
mnmsnrk <69619645+mnmsnrk@users.noreply.github.com>
2020-09-08 18:18:21 +0900

    バリデーションの追加と単体テストの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,16 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage
   belongs_to_active_hash :handling
   belongs_to_active_hash :prefecture
-  
-  validates :category_id,:condition_id,:postage_id,:prefecture_id,:handling_id, numericality: { other_than: 1 } 
 
   with_options presence:true do
-  validates :name, :image_text, :category_id,:condition_id,:postage_id,:prefecture_id,:handling_id,:price
+    validates :image
+    validates :name,length:{maximum: 40,message: "is too long"}
+    validates :image_text, length: {maximum: 1000, message: "is too long"}
+    validates :price,numericality: { with: /\A[0-9]+\z/, message: "は半角で入力してください。"},
+               inclusion: { in: 300..9_999_999, message: "Out of setting range"}
   end
+
+  validates :category_id,:condition_id,:postage_id,:prefecture_id,:handling_id, numericality: { other_than: 1 ,message: "Select"}
 
   belongs_to :user
   has_one_attached :image
