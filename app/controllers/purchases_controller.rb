5c0e2892aa5fe6a16505875150e27528cd7a37b0
mnmsnrk <69619645+mnmsnrk@users.noreply.github.com>
2020-09-23 19:35:17 +0900

    クレジットカードの登録と登録した情報を使って購入する

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,20 +6,26 @@ class PurchasesController < ApplicationController
   require 'payjp'
 
   def index
+    card_set
     @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new
+
   end
 
   def create
+    redirect_to new_card_path and return unless current_user.card.present?
+
+    card_set
     @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new(order_params)
-     if @item_purchase.valid?
+
+   if @item_purchase.valid?
       pay_item
-      @item_purchase.save
-        return redirect_to root_path
-      else
-        render 'index' 
-    end
+       @item_purchase.save
+       return redirect_to root_path
+    else
+         render 'index' 
+    end 
   end
 
    private
@@ -30,11 +36,13 @@ class PurchasesController < ApplicationController
 
    def pay_item
     @item = Item.find(params[:item_id])
+    card = Card.find_by(user_id: current_user.id)
      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+     customer_token = current_user.card.customer_token # ログインしているユーザーの顧客トークンを定義
      Payjp::Charge.create(
-       amount: @item.price,
-       card: order_params[:token],
-       currency:'jpy'
+      amount: @item.price, # 商品の値段
+      customer: card.customer_token, # 顧客のトークン
+      currency: 'jpy' # 通貨の種類（日本円）
      )
    end
 
@@ -43,5 +51,15 @@ class PurchasesController < ApplicationController
     if @item.purchase.present?
       redirect_to root_path
     end
+  end
+
+  def card_set
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
+    card = Card.find_by(user_id: current_user.id) # ユーザーのid情報を元に、カード情報を取得
+
+    redirect_to new_card_path and return unless card.present?
+    
+    customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
+    @card = customer.cards.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'cards/new'
+  get 'users/show'
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :purchases
+    resources :purchases 
   end
+  resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
 end
